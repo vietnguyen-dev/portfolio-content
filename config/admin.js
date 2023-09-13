845dfed37c234db9f862d363c595cc826f8f44b8
vietnguyen-dev <77704810+vietnguyen-dev@users.noreply.github.com>
2023-09-13 12:34:10 -0700

    hoping this works

--- config/admin.js
+++ config/admin.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET', 'boobies'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT', 'boobies'),
+    salt: env('API_TOKEN_SALT', crypto.randomBytes(16).toString('base64')),
   },
 
 });
